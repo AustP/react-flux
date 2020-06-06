1bfae59880ff340771f6908f7bfa128f981a8f62
Aust <austen@probsttech.com>
2020-06-06 10:49:39 -0600

    Add ts-jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // A preset that is used as a base for Jest's configuration.
+  preset: 'ts-jest',
+
+  // The test environment that will be used for testing.
+  testEnvironment: 'node',
+
+  // The glob patterns Jest uses to detect test files.
+  testMatch: ['**/tests/*.ts'],
+};
